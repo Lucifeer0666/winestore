0cc79c15b99983d5479bc63c480fae4a69fbfe2a
mouadhvkmi <57360750+mouadhvkmi@users.noreply.github.com>
2023-02-12 00:04:58 +0100

    Add files via upload

---
+++ save.php
@@ -0,0 +1,23 @@
+<?php
+	$Name = $_POST['Name'];
+	
+
+	$email = $_POST['email'];
+	$password = $_POST['password'];
+	
+
+	// Database connection
+	$conn = new mysqli('localhost','root','','user');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into registration(firstName, lastName, gender, email, password, number) values(?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sss", $Name,  $email, $password);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
